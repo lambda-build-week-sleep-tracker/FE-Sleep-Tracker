4856cbe2c565dbaa6a334a998f59b5dbf38487c7
Kyle Guerrero <kyleguerrero85@gmail.com>
2019-08-27 15:51:21 -0500

    Added in sign up and log in forms along with form HTML5 form validation for the sign up form

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import './index.css';
 
 // Importing components
 import LoginForm from './components/LoginForm/LoginForm';
+import SignUpForm from './components/signUpForm/SignUpForm';
+
 function App() {
   return (
     <div className="App">
       <p>App Component</p>
 			<LoginForm/>
+			<SignUpForm/>
     </div>
   );
 }
--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // Importing Styles
 import './loginForm.scss'
+import axios from 'axios';
 
 function LoginForm() {
+
+	const [user, setUser] = useState({userName: "", password: ""})
+
+	const changeHandler = event => {
+		console.log("TARGET NAME IS", event.target.name)
+		console.log(event.target.value)
+		setUser({...user, [event.target.name]: event.target.value})
+	}
+
+	const formSubmit = event => {
+		event.prevetDefault()
+		// axios.post(user)
+	}
 
 	return(
 		<div className="login-container">
@@ -12,11 +26,25 @@ function LoginForm() {
 				<form>
 				{/* User Name Input */}
 				<label htmlFor="userName">User Name</label>
-				<input type="text" name="userName" id="userName" required placeholder="John Doe"/>
+				<input 
+					type="text" 
+					name="userName" 
+					id="userName" 
+					required 
+					placeholder="John Doe"
+					onChange={changeHandler}
+					/>
 
 				{/* Password */}
 				<label htmlFor="password">Password</label>
-				<input type="password" name="password" id="password" required placeholder="Enter Password"/>
+				<input 
+					type="password" 
+					name="password" 
+					id="password" 
+					required 
+					placeholder="Enter Password"
+					onChange={changeHandler}
+					/>
 
 				<button type="submit">Submit</button>
 				</form>
