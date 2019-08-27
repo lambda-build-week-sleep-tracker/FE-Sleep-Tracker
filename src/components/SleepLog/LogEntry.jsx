800e67d46b0f322b46ecba0c2625bbdff81ac865
ElijahMcKay <45011056+ElijahMcKay@users.noreply.github.com>
2019-08-27 12:17:04 -0500

    created directory for SleepLog component

---
+++ src/components/SleepLog/LogEntry.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LogEntry = (props) => {
+    return ( 
+        <div>
+            <p>Entry</p>
+        </div>
+     );
+}
+ 
+export default LogEntry;
---
+++ src/components/SleepLog/SleepLog.jsx
@@ -0,0 +1,11 @@
+import React from 'react'; 
+
+const SleepLog = () => {
+    return ( 
+        <div>
+            <p>SleepLog Component</p>
+        </div>
+     );
+}
+ 
+export default SleepLog;
