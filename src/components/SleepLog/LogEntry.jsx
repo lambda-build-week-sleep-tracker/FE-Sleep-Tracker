72ceb4f1378557fbe4228e531e6737ed4dbfc83a
ElijahMcKay <45011056+ElijahMcKay@users.noreply.github.com>
2019-08-27 15:45:12 -0500

    initial styling finished on SleepLog finished, need to get data from backend to render the rest of log

--- src/components/SleepLog/LogEntry.jsx
+++ src/components/SleepLog/LogEntry.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+
+//importing styling
+import './LogEntry.scss'
 
 const LogEntry = (props) => {
+    
     return ( 
-        <div>
-            <p>Entry</p>
+        <div className="log-block-container">
+            <div className="log-block">
+                <input placeholder="Start Time" />
+                <hr className="time-splitter"/>
+                <input placeholder="End Time"/>
+            </div>
         </div>
      );
 }
