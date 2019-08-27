f7c2340802b9689eb1643f6fe995dcf5b34cfd27
ElijahMcKay <45011056+ElijahMcKay@users.noreply.github.com>
2019-08-27 18:00:14 -0500

    last merge for air table

--- src/App.js
+++ src/App.js
@@ -5,14 +5,12 @@ import './index.css';
 
 // Importing components
 import LoginForm from './components/LoginForm/LoginForm';
-import SleepLog from './components/SleepLog/SleepLog';
-
 
 function App() {
   return (
     <div className="App">
       <p>App Component</p>
-			<LoginForm/>
+			<LoginForm/> 
     </div>
   );
 }
