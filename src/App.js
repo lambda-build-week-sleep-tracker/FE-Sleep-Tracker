df441f53a6d8b1433237242b4d81e2ce4daee3d5
Bishop Lake <lake.d.bishop@gmail.com>
2019-08-28 11:00:04 -0500

    home componet readded

--- src/App.js
+++ src/App.js
@@ -7,14 +7,16 @@ import './index.css';
 import LoginForm from './components/LoginForm/LoginForm';
 import SignUpForm from './components/signUpForm/SignUpForm';
 import SleepLog from './components/SleepLog/SleepLog';
+import Home from './components/Home/Home'
 
 
 function App() {
   return (
     <div className="App">
       <p>App Component</p>
-			<LoginForm/> 
-			<SignUpForm/>
+      <Home />
+      {/* <LoginForm/> 
+			<SignUpForm/> */}
     </div>
   );
 }
