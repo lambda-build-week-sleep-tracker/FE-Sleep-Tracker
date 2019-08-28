70c035820d3fa6de5a49e9aa03efc624d0d56367
ElijahMcKay <45011056+ElijahMcKay@users.noreply.github.com>
2019-08-28 11:44:42 -0500

    login/signup authentication completed until we implement Formik forms

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -4,35 +4,60 @@ import axios from 'axios';
 import './loginForm.scss'
 
 
-function LoginForm() {
+function LoginForm(props) {
 
-	const [user, setUser] = useState({userName: "", password: ""})
+	const [user, setUser] = useState({email: "", password: ""})
 
 	const changeHandler = event => {
-		console.log("TARGET NAME IS", event.target.name)
-		console.log(event.target.value)
-		setUser({...user, [event.target.name]: event.target.value})
+		// console.log("THIS IS THE EVENT VALUE ", event.target.value)
+		// console.log("THIS IS THE NAME OF THE TARGET", event.target.name)
+		setUser({
+			...user, 
+			[event.target.name]: event.target.value
+		})
+		console.log(user); 
 	}
 
-	const formSubmit = event => {
-		event.prevetDefault()
-		// axios.post(user)
-	}
+	const submitForm = event => {
+		event.preventDefault()
 
-	return(
+		// const sentUser = {
+		// 	id: uuid(),
+		// 	...newUser, 
+		// }
+		// console.log(sentUser); 
+		console.log(user); 
+		axios
+			.post('https://sleeptracker-api.herokuapp.com/auth/login/', user)
+			.then((res) => {
+				localStorage.setItem('token', res.data.token)
+				// props.history.push('/bubbles'); 
+				console.log(res);
+				setUser({
+					email: '',
+					password: ''
+				})
+			})
+			.catch(err => {
+				console.log('Something went wrong', err)
+			})
+		}
+
+	return (
 		<div className="login-container">
 			<h2>Login</h2>
 			<div className="form-container">
-				<form>
+				<form onSubmit={submitForm}>
 				{/* User Name Input */}
-				<label htmlFor="userName">User Name</label>
+				<label htmlFor="email">User Name</label>
 				<input 
 					type="text" 
-					name="userName" 
+					name="email" 
 					id="userName" 
 					required 
 					placeholder="John Doe"
 					onChange={changeHandler}
+					value={user.email}
 					/>
 
 				{/* Password */}
@@ -44,9 +69,10 @@ function LoginForm() {
 					required 
 					placeholder="Enter Password"
 					onChange={changeHandler}
+					value={user.password}
 					/>
 
-				<button type="submit" onClick={formSubmit}>Submit</button>
+				<button type="submit">Submit</button>
 				</form>
 			</div>
 			
