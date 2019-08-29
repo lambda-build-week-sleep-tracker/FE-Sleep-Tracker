47fd8755ea5c053a941d955d31f7561faa823880
ElijahMcKay <45011056+ElijahMcKay@users.noreply.github.com>
2019-08-28 19:38:25 -0500

    test

--- src/App.js
+++ src/App.js
@@ -7,30 +7,15 @@ import './index.css';
 import LoginForm from './components/LoginForm/LoginForm';
 import SignUpForm from './components/signUpForm/SignUpForm';
 import SleepLog from './components/SleepLog/SleepLog';
-import Home from './components/Home/Home'
-import Nav from './components/Nav/Nav'
 
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <Nav />
-      <Home />
-      {/* <LoginForm/> 
-			<SignUpForm/> */}
-=======
         <p>App Component</p>
-<<<<<<< HEAD
         {/* <LoginForm/> 
         <SignUpForm/> */}
         <SleepLog />
-=======
-        <LoginForm/> 
-        <SignUpForm/>
-        {/* <SleepLog /> */}
->>>>>>> 1556c0c8fab32fe56bb2d2c87e39399a12e7701d
->>>>>>> origin/master
     </div>
   );
 }
