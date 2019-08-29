ede97f8255584bb624e6821f5b4226a5286aa48a
Preston <pres.10.nap@gmail.com>
2019-08-29 11:01:47 -0400

    yeah it brokeded

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import './index.css';
-
+import ClockApp from './components/clock/Clock'
 // Importing components
-import LoginForm from './components/LoginForm';
+// import LoginForm from './components/LoginForm';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <p>App Component</p>
-			<LoginForm/>
-=======
         <p>App Component</p>
         {/* <LoginForm/> 
         <SignUpForm/> */}
-        <SleepLog />
->>>>>>> b28fc5eecb44e966357b624e5046724e837b8215
+        <ClockApp />
     </div>
   );
 }
--- src/components/clock/Clock.jsx
+++ src/components/clock/Clock.jsx
@@ -1,112 +1,116 @@
-import React, { Component, useState } from 'react';
+import React, { Component } from 'react';
 import './Clock.scss';
 import axios from 'axios';
+import DigClock from './DigClock'
 
 // Digital Clock
 // let time = new Date().toLocaleString();  
 
-let timerStart = [];
+let startTime = [];
 let timerEnd = [];
 // let timerTotal = [];
-
-class DigClock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      hour: new Date().getHours().toLocaleString(),
-      minute: new Date().getMinutes().toLocaleString(),
-    }
-  }
-
-  componentDidMount() {
-    this.intervalID = setInterval(
-      () => this.tick(),
-      1000
-    );
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.intervalID);
-  }
-
-  tick() {
-    this.setState({
-      hour: new Date().getHours().toLocaleString(),
-      minute: new Date().getMinutes().toLocaleString()
-    });
-  }
-
-  render() {
-    return (
-      <p className='AppClock'>
-        {this.state.hour}:{this.state.minute}
-      </p>
-    )
-  }
-}
-
-
-function startTimer(obj) {
-  let hour = new Date().getHours().toLocaleString();
-  let minute = new Date().getMinutes().toLocaleString();
-  timerStart = `${hour}:${minute}`;
-  console.log(timerStart);
-  obj = timerStart;
-  return (obj);
-}
-
-function endTimer(obj) {
-  let hour = new Date().getHours().toLocaleString();
-  let minute = new Date().getMinutes().toLocaleString();
-  timerEnd = `${hour}:${minute}`;
-  console.log(timerEnd);
-  console.log(timerStart);
-  obj = timerEnd;
-  return (obj);
-}
-
-function logTimer(obj1, obj2) {
-  axios.post(`https://reqres.in/api/users/`, { obj1, obj2 })
-    .then(res => {
-      console.log(res.data);
-    })
-}
 
 class ClockApp extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      // hour: new Date().getHours().toLocaleString(),
+      // minute: new Date().getMinutes().toLocaleString(),
       startTime: null,
       endTime: null
     }
   }
+
+  formatHour(hour) {
+    if (hour > 12){
+      return (hour-12);
+    } else if (hour === 0){
+      return (hour + 12);
+    } else {
+      return (hour);
+    }
+  }
+  
+  formatMinute(minute) {
+    if (minute < 10){
+      return ("0"+minute);
+    }
+    else {
+      return (minute);
+    }
+  }
+
+  startTimer() {
+    let hour = new Date().getHours().toLocaleString();
+    let minute = new Date().getMinutes().toLocaleString();
+    this.setState({
+      startTime:`${this.formatHour(hour)}:${this.formatMinute(minute)}`
+    });
+    console.log(this.state.startTime);
+    
+  }
+  
+  endTimer(obj) {
+    let hour = new Date().getHours().toLocaleString();
+    let minute = new Date().getMinutes().toLocaleString();
+    this.setState({
+      ...this.state,
+      endTime:`${this.formatHour(hour)}:${this.formatMinute(minute)}`
+  })
+    // this.setState.endTime = `${hour}:${minute}`;
+    console.log(this.setState.endTime);
+    console.log(startTime);
+    obj = timerEnd;
+    return (obj);
+  }
+  
+  logTimer(startTime, timerStop) {
+    axios.post(`https://reqres.in/api/users/`, { startTime, timerStop })
+      .then(res => {
+        // this.setState.startTime;
+        console.log(this.state)
+        console.log(res.data);
+      })
+      .catch(err =>{
+        console.log(err)
+      })
+  }
+    componentDidUpdate(prevProps) {
+      // Typical usage (don't forget to compare props):
+      if (this.props.userID !== prevProps.userID) {
+        this.fetchData(this.props.userID);
+      }
+    }
+    
   render() {
     return (
       <div className="App">
         <div className="Row">
         <div className="Start">
           <h2>Start</h2>
-          <DigClock className="StartClock" />
+          {/* <DigClock className="StartClock" /> */}
+          <p>{this.state.startTime}</p>
         </div>
         <div className="Awake">
           <h2>Awake</h2>
-          <DigClock className="AwakeClock"/>
+          {/* <DigClock className="AwakeClock"/> */}
+          <p>{this.state.endTime}</p>
         </div>
         </div>
-        <DigClock />
+        <DigClock props= {this.state} />
         
-        <button className="StartTimer" onClick={() =>
-          startTimer(this.state.startTime)
-        }>
+        <button className="StartTimer" onClick={() => this.startTimer()}>
           Start Sleep Timer
             </button>
 
-        <button className="EndTimer" onClick={() => endTimer(this.state.endTime)
+        <button className="EndTimer" onClick={() => 
+          this.endTimer()
         }>
           End Sleep Timer
           </button>
 
-        <button className="LogTime" onClick={() => logTimer(this.state.startTime, this.state.endTime)
+        <button className="LogTime" onClick={() => 
+        this.state.logTimer(this.state.startTime, this.state.endTime)
         }>
           Log Sleep Time
           </button>
