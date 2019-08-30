f9cf26f95d93f923122d342c7624d2d56097b514
Jonathan Taylor <jonyonson@icloud.com>
2019-08-30 04:37:17 -0500

    fixed styling issue with login and signup forms

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
 import React from 'react';
 // import './index.css';
-import { PrivateRoute } from './components/PrivateRoute/PrivateRoute'; 
-import { Link, Route } from 'react-router-dom' 
-
-
+import { PrivateRoute } from './components/PrivateRoute/PrivateRoute';
+import { Link, Route } from 'react-router-dom';
 
 // Importing components
 import LoginForm from './components/LoginForm/LoginForm';
 import SignUpForm from './components/signUpForm/SignUpForm';
 import SleepLog from './components/SleepLog/SleepLog';
-import Home from './components/Home/Home'
-import Nav from './components/Nav/Nav'
-import Clock from './components/clock/Clock'
-import SleepLogModal from './components/SleepLog/SleepLogModal';
-
+import Home from './components/Home/Home';
+import Nav from './components/Nav/Nav';
+import Clock from './components/clock/Clock';
+// import SleepLogModal from './components/SleepLog/SleepLogModal';
 
 function App() {
   return (
     <div className="App">
-       <Nav />
-      <Route exact path='/' component={LoginForm} />
-      <Route path='/signup' component={SignUpForm} />
-      <PrivateRoute path='/timer' component={Clock} />
-      <PrivateRoute path='/home' component={Home} />
-      <PrivateRoute path='/sleeplog' component={SleepLog} />
+      <Nav />
+      <Route exact path="/" component={LoginForm} />
+      <Route path="/signup" component={SignUpForm} />
+      <PrivateRoute path="/timer" component={Clock} />
+      <PrivateRoute path="/home" component={Home} />
+      <PrivateRoute path="/sleeplog" component={SleepLog} />
     </div>
   );
 }
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,24 +1,29 @@
 import React from 'react';
-import './nav.scss'
-import { Link } from 'react-router-dom'; 
+import './nav.scss';
+import { Link } from 'react-router-dom';
 
 function Nav(props) {
+  const logout = () => {
+    localStorage.removeItem('token');
+    props.props.history.push('/');
+  };
+  return (
+    <div className="nav-container">
+      <h3>Rest Easy</h3>
 
-    const logout = () => {
-        localStorage.removeItem('token'); 
-        props.props.history.push('/'); 
-    }
-    return (
-        <div className='nav-container'>
-            <h3>Sleep Tacker</h3>
-            
-            <nav>
-                <Link to='/timer' className='navlink'>Sleep Timer</Link>
-                <Link to='/sleeplog' className="navlink">Sleep Log</Link>
-                <a className="navlink" onClick={logout}>Log Out</a>
-            </nav>
-        </div>
-    )
+      <nav>
+        <Link to="/timer" className="navlink">
+          Sleep Timer
+        </Link>
+        <Link to="/sleeplog" className="navlink">
+          Sleep Log
+        </Link>
+        <a className="navlink" onClick={logout}>
+          Log Out
+        </a>
+      </nav>
+    </div>
+  );
 }
 
-export default Nav 
+export default Nav;
