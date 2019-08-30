3a83046bded3099e2579f0bc724b9fb1688a33b7
Kyle Guerrero <kyleguerrero85@gmail.com>
2019-08-30 03:40:52 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,13 +18,12 @@ import SleepLogModal from './components/SleepLog/SleepLogModal';
 function App() {
   return (
     <div className="App">
-      {/* <Nav />
+       <Nav />
       <Route exact path='/' component={LoginForm} />
       <Route path='/signup' component={SignUpForm} />
       <PrivateRoute path='/timer' component={Clock} />
       <PrivateRoute path='/home' component={Home} />
-			<PrivateRoute path='/sleeplog' component={SleepLog} /> */}
-			<SignUpForm/>
+      <PrivateRoute path='/sleeplog' component={SleepLog} />
     </div>
   );
 }
