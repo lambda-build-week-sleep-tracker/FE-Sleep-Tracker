f58b98c6f20a30c936d89d1cd913880e1297e6a7
Bishop Lake <lake.d.bishop@gmail.com>
2019-08-30 02:31:09 -0500

    added summery with childs name form local storage to the home componet

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './home.scss'
 import { Link } from 'react-router-dom'; 
 import Graph from '../Graph/Graph'
 
 function Home() {
+    
+    const [childName, setChildName] = useState(localStorage.getItem(childObj.child_name))
+    
     return (
         <div className="home">
             <div className='log-time'>Log New Sleep Time</div>
             <Link to='/sleeplog'>My Sleep Log</Link>
-            {/* <a href="">Suggested Sleep Schedule</a> */}
+            <p>{`${childName}' Tends to be in the best mood when they have slept for about 10hrs`}</p>
             <div className="graph-container">
                 {/* <input type="text"/> */}
                 <Graph />
